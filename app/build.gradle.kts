6e7c0c70a4bdfba461df43ac5ac95b45425061c5
cma58 <amine.chtaiti@gmail.com>
2026-06-04 00:13:12 +0200

    Bump private build version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.example.carvideo"
         minSdk = 29
         targetSdk = 37
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 2
+        versionName = "1.1-private"
     }
 
     signingConfigs {
